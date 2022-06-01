41f59848528c12fbe724f2ce4545217583bf1225
VantaStrix88 <VantaStrix88@Ryzen>
2022-05-31 23:41:33 -0300

    Atualizaões dia 5 de 7

--- src/main/java/br/com/alura/sevendaysofcode/IMDBJsonParser.java
+++ src/main/java/br/com/alura/sevendaysofcode/IMDBJsonParser.java
@@ -19,22 +19,13 @@ public class IMDBJsonParser {
         String[] obras = json.replace("},{", "=").split("=");
         List<Filme> filmes = new ArrayList<>();
         for (String obra : obras) {
-            //System.out.println(filme);
-            
-            /* titulos.add(obra.substring(obra.indexOf("\"title\"") + 9, obra.indexOf(",\"fullTitle\"") - 1));
-            anos.add(obra.substring(obra.indexOf("\"year\"") + 8, obra.indexOf(",\"image\"") - 1));
-            notas.add(obra.substring(obra.indexOf("\"imDbRating\"") + 14, obra.indexOf(",\"imDbRatingCount\"") - 1));
-            imagens.add(obra.substring(obra.indexOf("\"image\"") + 9, obra.indexOf(",\"crew\"") - 1)); */
-
+  
             String titulo = obra.substring(obra.indexOf("\"title\"") + 9, obra.indexOf(",\"fullTitle\"") - 1);
             String ano = obra.substring(obra.indexOf("\"year\"") + 8, obra.indexOf(",\"image\"") - 1);
             String nota = obra.substring(obra.indexOf("\"imDbRating\"") + 14, obra.indexOf(",\"imDbRatingCount\"") - 1);
             String posterURL = obra.substring(obra.indexOf("\"image\"") + 9, obra.indexOf(",\"crew\"") - 1);
             
             filmes.add(new Filme(titulo, posterURL, nota, ano));
-            //System.out.println(obra.substring(obra.indexOf("\"image\"") + 9, obra.indexOf(",\"crew\"") - 1));
-            //System.out.println("Título: " + titulos.get(titulos.size() - 1) + " - Ano: " + anos.get(anos.size() - 1) + " - Nota: " + notas.get(notas.size() - 1));
-            //System.out.println(filmes.get(filmes.size() - 1).getTitulo());
         }
         System.out.println(filmes.size());
         return filmes;
--- src/main/java/br/com/alura/sevendaysofcode/Sevendaysofcode.java
+++ src/main/java/br/com/alura/sevendaysofcode/Sevendaysofcode.java
@@ -10,10 +10,6 @@ package br.com.alura.sevendaysofcode;
  * @author VantaStrix88
  */
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.*;
-import java.util.ArrayList;
 import java.util.List;
 import br.com.alura.sevendaysofcode.Filme;
 import java.io.FileNotFoundException;
@@ -21,17 +17,12 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Sevendaysofcode {
 
     public static void main(String[] args) {
-        //System.out.println(response.body());
-        
-        //String body = response.body();
         String body = IMDBApiClient.getResponse("https://imdb-api.com/en/API/Top250Movies/k_5cpmf1wr");
-        List<Filme> filmes = IMDBJsonParser.parse(body);
-        
+        List<Filme> filmes = IMDBJsonParser.parse(body);        
         try (PrintWriter writer = new PrintWriter("filmes.html", "utf-8")) {
             HTMLGenerator generator = new HTMLGenerator(writer);
             generator.generate(filmes);
