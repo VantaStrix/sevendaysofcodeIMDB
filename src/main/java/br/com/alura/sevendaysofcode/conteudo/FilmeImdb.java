4922780a1e11112cd920830dd4ce4d567201587a
VantaStrix88 <VantaStrix88@Ryzen>
2022-06-11 18:18:23 -0300

    Melhorias em formatação e nomenclatura.

---
+++ src/main/java/br/com/alura/sevendaysofcode/conteudo/FilmeImdb.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.alura.sevendaysofcode.conteudo;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author VantaStrix88
+ */
+public final class FilmeImdb implements Conteudo {
+    
+    private final String titulo;
+    private final String posterURL;
+    private final String rating;
+    private final String ano;
+
+    public FilmeImdb(String titulo, String posterURL, String rating, String ano) {
+        this.titulo = titulo;
+        this.posterURL = posterURL;
+        this.rating = rating;
+        this.ano = ano;
+    }
+
+    @Override
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String getPosterURL() {
+        return posterURL;
+    }
+
+    @Override
+    public String getRating() {
+        return rating;
+    }
+
+    @Override
+    public String getAno() {
+        return ano;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.titulo);
+        hash = 89 * hash + Objects.hashCode(this.posterURL);
+        hash = 89 * hash + Objects.hashCode(this.rating);
+        hash = 89 * hash + Objects.hashCode(this.ano);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FilmeImdb other = (FilmeImdb) obj;
+        if (!Objects.equals(this.titulo, other.titulo)) {
+            return false;
+        }
+        if (!Objects.equals(this.posterURL, other.posterURL)) {
+            return false;
+        }
+        if (!Objects.equals(this.rating, other.rating)) {
+            return false;
+        }
+        return Objects.equals(this.ano, other.ano);
+    }
+    
+}
