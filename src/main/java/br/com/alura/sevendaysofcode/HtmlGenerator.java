96a96cabeeb8158b26b8e137990cdb6dc7759347
VantaStrix88 <VantaStrix88@Ryzen>
2022-06-11 18:14:18 -0300

    Melhorias em formatação e nomenclatura.

---
+++ src/main/java/br/com/alura/sevendaysofcode/HtmlGenerator.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.alura.sevendaysofcode;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import br.com.alura.sevendaysofcode.conteudo.Conteudo;
+
+/**
+ *
+ * @author VantaStrix88
+ */
+public class HtmlGenerator {
+
+    private final PrintWriter writer;
+    
+    public HtmlGenerator (PrintWriter writer) {
+        this.writer = writer;
+    }
+    
+    public void generate(List<? extends Conteudo> conteudo) {
+        String head =
+                """
+                <head>
+                    <meta charset=\"utf-8\">
+                    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+                    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" 
+                        + "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
+                </head>
+                """;
+        String divTemplate =
+                """
+                <div class=\"card text-white bg-dark mb-3\" style=\"max-width: 18rem;\">
+                    <h4 class=\"card-header\">%s</h4>
+                    <div class=\"card-body\">
+                	<img class=\"card-img\" src=\"%s\" alt=\"%s\">
+                	<p class=\"card-text mt-2\">Rating: %s - Ano: %s</p>
+                    </div>
+                </div>
+                """;
+        this.writer.println("<html>");
+        this.writer.println(head);
+        this.writer.println("<body>");
+        for (var obra : conteudo) {
+            this.writer.println(String.format(divTemplate, obra.getTitulo(),
+                    obra.getPosterURL(), obra.getTitulo(), obra.getRating(),
+                    obra.getAno()));
+        }
+        this.writer.println("</body>" + System.lineSeparator() + "</html>");
+    }
+}
